57edbf3be7dd7ba9b7af3835c77b2f41e22ed1b0
PhilipSoltys1 <soltysp@sheridancollege.ca>
2024-11-12 21:16:14 -0500

    Commit

--- Desktop/Software_design/project/TechTeam_BlackJack/src/ca/sheridancollege/project/Player.java
+++ Desktop/Software_design/project/TechTeam_BlackJack/src/ca/sheridancollege/project/Player.java
@@ -11,17 +11,24 @@ package ca.sheridancollege.project;
  * @author dancye
  * @author Paul Bonenfant Jan 2020
  */
-public abstract class Player {
+    public abstract class Player {
 
     private String name; // the unique name for this player
+    private List<Card> hand; // the player's current hand of cards
+    private double balance; // the player's current balance
+    private double currentBet; // the player's current bet amount
+    private boolean isStanding; // flag to track if the player has chosen to stand
 
     /**
      * A constructor that allows you to set the player's unique ID
      *
      * @param name the unique ID to assign to this player.
      */
-    public Player(String name) {
+    public Player(String name, double balance) {
         this.name = name;
+        this.balance = balance;
+        this.hand = new ArrayList<>();
+        this.isStanding = false;
     }
 
     /**
@@ -32,12 +39,59 @@ public abstract class Player {
     }
 
     /**
-     * Ensure that the playerID is unique
+     * Ensure that the player name is unique
      *
      * @param name the player name to set
      */
     public void setName(String name) {
         this.name = name;
+    }
+
+    /**
+     * Places a bet for the player
+     *
+     * @param amount the amount to bet
+     */
+    public void placeBet(double amount) {
+        this.currentBet = amount;
+        this.balance -= amount;
+    }
+
+    /**
+     * Receives a card and adds it to the player's hand
+     *
+     * @param card the card to receive
+     */
+    public void receiveCard(Card card) {
+        hand.add(card);
+    }
+
+    /**
+     * Decides the player's move
+     *
+     * @param action the action to take ("Hit" or "Stand")
+     */
+    public void decideMove(String action) {
+        if ("Hit".equalsIgnoreCase(action)) {
+            // Game will provide another card, logic defined in the game class
+        } else if ("Stand".equalsIgnoreCase(action)) {
+            isStanding = true;
+        }
+    }
+
+    /**
+     * Clears the player's hand at the end of a round
+     */
+    public void clearHand() {
+        hand.clear();
+        isStanding = false;
+    }
+
+    /**
+     * @return true if the player is standing, false otherwise
+     */
+    public boolean isStanding() {
+        return isStanding;
     }
 
     /**
@@ -47,4 +101,3 @@ public abstract class Player {
     public abstract void play();
 
 }
-
