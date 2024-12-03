004ab18332712fc9845b6620dab5948e59d9166d
MDGuzma <degumark@sheridancollege.ca>
2024-12-03 00:00:38 -0500

    Made changes to card.java, + Card & Suit values + Changed to using Card enum reference from curriculum

--- Desktop/Software_design/project/TechTeam_BlackJack/src/ca/sheridancollege/project/Card.java
+++ Desktop/Software_design/project/TechTeam_BlackJack/src/ca/sheridancollege/project/Card.java
@@ -12,20 +12,97 @@ package ca.sheridancollege.project;
  * @author dancye
  */
 public abstract class Card {
-    private String rank;
-    private String suit;
-    private int value;
+    public enum Suit {HEARTS, CLUBS, SPADES, DIAMONDS}
+    public enum Value {ACE,TWO,THREE,FOUR,FIVE,SIX,SEVEN,EIGHT,NINE,TEN,JACK,QUEEN,KING,JOKER}
+    
+    private Value value;
+    private Suit suit;
 
+//    TODO
+//    Switch statement to assign values to cards
+//    Logic for ACE = 1 || 11
+    
+//   DONE
+//    Suits have value of 10
+   
+    
+    
     // Constructors, getters, and setters would go here
-
-    public int getValue() {
+    
+    // Constructor
+    public Card(Value value, Suit suit) {
+        this.value = value;
+        this.suit = suit;
+    }
+    
+    public Value getValue() {
         // Method implementation
-        return 0;
+        return value;
     }
 
+    /**
+     * @param value the value to set
+     */
+    public void setValue(Value value) {
+        this.value = value;
+    }
+    
+    /**
+     * @return the suit
+     */
+    public Suit getSuit() {
+        return suit;
+    }
+
+    
+    /**
+     * @param suit the suit to set
+     */
+    public void setSuit(Suit suit) {
+        this.suit = suit;
+    }
+//    Method to retun appropriate value for card
+    public int getNumericValue(){
+//        Switch Statement 
+        switch(value){
+            case ACE: 
+                return 1;
+//                Could probably do if true = 1 || if false = 11
+            case TWO:
+                return 2;
+            case THREE:
+                return 3;
+            case FOUR:
+                return 4;
+            case FIVE:
+                return 5;
+            case SIX:
+                return 6;
+            case SEVEN:
+                return 7;
+            case EIGHT:
+                return 8;
+            case NINE:
+                return 9;
+            case TEN:
+            case JACK:
+            case QUEEN:
+            case KING:
+                return 10;
+            default:
+                throw new IllegalArgumentException("Unexpected value: " + value);
+        }
+    }
+    
+        // Method to make all suits = 10
+    public int getSuitValue() {
+        return 10; // All suits are considered to have a value of 10
+    }
+    
+    
+    
     @Override
     public String toString() {
-        // Method implementation
-        return "";
+        return value + " of " + suit;
     }
 }
